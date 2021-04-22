7032e34752a06372f1819d75944a19b4cbc85410
Muhammad <muhammad.safarov@web.de>
2021-04-23 00:41:16 +0200

    Finished logic

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 "use strict"
 
+const questionElement = document.getElementById("question");
+const quiz = document.getElementById("quiz");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+const rButtons = document.querySelectorAll(".answer");
+
+
 const quizData = [
   {
     question: "How old am I?",
@@ -42,3 +52,57 @@ const quizData = [
     correct:"d",
   },
 ];
+
+let currentQuestion = 0;
+let score = 0;
+
+
+loadQuiz();
+
+function loadQuiz(){
+  deselectAnswers();
+  const currentQuizData = quizData[currentQuestion];
+
+  questionElement.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function checkSelected (){
+  for (var button of rButtons) {
+    if(button.checked){
+      return button.id;
+    }
+  }
+  return undefined;
+
+}
+
+function deselectAnswers(){
+  for (var button of rButtons) {
+    button.checked =false;
+  }
+
+}
+
+submitBtn.addEventListener("click",()=>{
+
+  const answer = checkSelected();
+
+  if(answer){
+    if(answer == quizData[currentQuestion].correct){
+      score++;
+    }
+    currentQuestion++;
+    if(currentQuestion<= quizData.length-1){
+      loadQuiz();
+    }else{
+      quiz.innerText = "You have answered correctly at "+ score + "/" + quizData.length + " questions"
+      quiz.innerHTML = ' ${quiz}'
+    }
+  }
+
+
+})
