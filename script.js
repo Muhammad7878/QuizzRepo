4673be48f41cd7763026c0d8bab24b628eb542e4
Muhammad <muhammad.safarov@web.de>
2021-04-22 22:57:31 +0200

    Added a layout and CSS

--- script.js
+++ script.js
@@ -8,5 +8,37 @@ const quizData = [
     c: 43,
     d: 32,
     correct: "b"
-  }
-]
+  },
+  {
+    question:"What is the most used programming language in 2019",
+    a:"Java",
+    b:"C",
+    c:"Scala",
+    d:"JavaScript",
+    correct:"a"
+  },
+  {
+    question:"Who is the President of USA",
+    a:"Me",
+    b:"Barak Obama",
+    c:"Jenifer Aniston",
+    d:"Brad Pitt",
+    correct:"b"
+  },
+  {
+    question:"What does HTML stands for",
+    a:"Hypertext Markup Language",
+    b:"Harry Torter Making love",
+    c:"How to make love",
+    d:"How to moan louder",
+    correct:"a"
+  },
+  {
+    question:"What year was JavaScript launched",
+    a:"2019",
+    b:"2000",
+    c:"1995",
+    d:"none of above",
+    correct:"d",
+  },
+];
